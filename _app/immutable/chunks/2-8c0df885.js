091437cde1a0a41a6757845c5f6bbd8a30eb44fb
xKesvaL <jabeddou@gmail.com>
2022-12-14 15:14:18 +0100

    Updates

---
+++ _app/immutable/chunks/2-8c0df885.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/(mains)/_page.svelte-51897b21.js";export{t as component};
--- _app/immutable/chunks/2-d4988f34.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/(mains)/_page.svelte-86310841.js";export{t as component};
